05f30a578163adc5959032418344308540d85b62
Kledioz <claudiobo1122@gmail.com>
2019-11-21 21:53:38 -0700

    Termine las acciones

--- Proyecto/src/main/java/mx/itson/taki/acciones/Acciones.java
+++ Proyecto/src/main/java/mx/itson/taki/acciones/Acciones.java
@@ -1,8 +1,20 @@
 package mx.itson.taki.acciones;
 
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.dateTime;
+import java.awt.Color;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
-import java.util.HashMap;
-import javax.swing.Icon;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -38,33 +50,89 @@ public class Acciones {
     }
 
     public void apretarBoton() {
+        try {
+            Thread.sleep(500l);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Acciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         if (txtInput.getText().length() > 3) {
             cargado = OpenWeather.obtener(txtInput.getText());
-            mostrarInformacion();
+            if (cargado != null) {
+                mostrarInformacion();
+            } else {
+                JOptionPane.showMessageDialog(null, "La ciudad ingresada no existe", "Error", JOptionPane.ERROR_MESSAGE);
+            }
         } else {
             JOptionPane.showMessageDialog(null, "Debes introducir mas de 3 caracteres", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     public void mostrarInformacion() {
-        lblUbicacion.setText(cargado.getCiudad()+" "+cargado.getTiempo().getCountry());
-        lblTemp.setText(cargado.getInfo().getTemp()+"");
-        
-        lblPresion.setText(cargado.getInfo().getPressure()+" hpa");
-        lblViento.setText(cargado.getViento().getSpeed()+" m/s");
-        lblHumedad.setText(cargado.getInfo().getHumidity()+"%");
-        lblDireccion.setText(cargado.getViento().getDeg()+"°");
-        lblMinTemp.setText(cargado.getInfo().getTempMin()+" °c");
-        lblMaxTemp.setText(cargado.getInfo().getTempMax()+" °c");
-        
+        lblUbicacion.setText(cargado.getCiudad() + " " + cargado.getTiempo().getCountry());
+        lblTemp.setText(cargado.getInfo().getTemp() + "");
+
+        lblPresion.setText(cargado.getInfo().getPressure() + " hpa");
+        lblViento.setText(cargado.getViento().getSpeed() + " m/s");
+        lblHumedad.setText(cargado.getInfo().getHumidity() + "%");
+        lblDireccion.setText(cargado.getViento().getDeg() + "°");
+        lblMinTemp.setText(cargado.getInfo().getTempMin() + " °c");
+        lblMaxTemp.setText(cargado.getInfo().getTempMax() + " °c");
+        lblNubes.setText(cargado.getNubes().getAll() + "%");
+
         lblEstado.setText(cargado.getClima().get(0).getMain());
-        icono.setIcon(new ImageIcon("https://openweathermap.org/img/wn/"+cargado.getClima().get(0).getIcon()+"@2x.png"));
-        
-        lblHoraLocal.setText(new Date().toString());
-        lblZonaHoraria.setText(cargado.getZonahoraria()+"");
-        lblSalidaSol.setText(new Date(cargado.getTiempo().getSunrise()).toString());
-        lblPuestaSol.setText(new Date(cargado.getTiempo().getSunset()).toString());
-        
+
+        URL url = null;
+        try {
+            url = new URL("https://openweathermap.org/img/wn/" + cargado.getClima().get(0).getIcon() + "@2x.png");
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Acciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Image image = null;
+        try {
+            image = ImageIO.read(url);
+        } catch (IOException ex) {
+            Logger.getLogger(Acciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        icono.setIcon(new ImageIcon(image));
+
+        SimpleDateFormat formato = new SimpleDateFormat("HH:mm");
+        formato.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        Date ahora = new Date((new Date().getTime()) + (cargado.getZonahoraria() * 1000));
+        Date sunrise = new Date((cargado.getTiempo().getSunrise() * 1000) + (cargado.getZonahoraria() * 1000));
+        Date sunset = new Date((cargado.getTiempo().getSunset() * 1000) + (cargado.getZonahoraria() * 1000));
+        lblHoraLocal.setText(formato.format(ahora));
+        lblZonaHoraria.setText("(GMT " + (cargado.getZonahoraria() / 60 / 60) + ")");
+
+        lblSalidaSol.setText(formato.format(sunrise));
+        lblPuestaSol.setText(formato.format(sunset));
+
+        int ahoraHoras = Integer.valueOf(lblHoraLocal.getText().split(":")[0]);
+
+        if (ahoraHoras <= 5) {
+            main.pnlInformacion.setColor1(new Color(33, 31, 112));
+            main.pnlInformacion.setColor2(new Color(3, 0, 40));
+        } else if (ahoraHoras > 6 && ahoraHoras <= 8) {
+            main.pnlInformacion.setColor1(new Color(243, 195, 0));
+            main.pnlInformacion.setColor2(new Color(137, 36, 6));
+        } else if (ahoraHoras > 8 && ahoraHoras <= 13) {
+            main.pnlInformacion.setColor1(new Color(160, 229, 255));
+            main.pnlInformacion.setColor2(new Color(2, 120, 255));
+        } else if (ahoraHoras > 13 && ahoraHoras <= 16) {
+            main.pnlInformacion.setColor1(new Color(243, 195, 0));
+            main.pnlInformacion.setColor2(new Color(137, 36, 6));
+        } else if (ahoraHoras > 16 && ahoraHoras <= 19) {
+            main.pnlInformacion.setColor1(new Color(254, 126, 0));
+            main.pnlInformacion.setColor2(new Color(57, 31, 151));
+        } else if (ahoraHoras > 19) {
+            main.pnlInformacion.setColor1(new Color(33, 31, 112));
+            main.pnlInformacion.setColor2(new Color(3, 0, 40));
+        }
+
+        main.pnlInformacion.actualizar();
+
     }
 
 }
