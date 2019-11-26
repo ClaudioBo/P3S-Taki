595c2df9475b3242377d48b134164e8d4d6af7f3
hAL-09 <hAL-09@alfonso_sanches@live.com.mx>
2019-11-25 21:22:59 -0600

    Se documentaron 6 clases.

--- Proyecto/src/main/java/mx/itson/taki/entidades/Clima.java
+++ Proyecto/src/main/java/mx/itson/taki/entidades/Clima.java
@@ -5,18 +5,34 @@ public class Clima {
     private String main;
     private String icon;
 
+    /**
+     *Obtiene la descripcion del clima.
+     * @return La descripcion del clima.
+     */
     public String getMain() {
         return main;
     }
 
+    /**
+     * Cambia la descripcion del clima. 
+     * @param main La descripcion del clima.
+     */
     public void setMain(String main) {
         this.main = main;
     }
 
+    /**
+     * Obtiene el nombre del icono.
+     * @return El nombre del icono.
+     */
     public String getIcon() {
         return icon;
     }
 
+    /**
+     * Cambia el nombre del icono.
+     * @param icon El nombre del icono.
+     */
     public void setIcon(String icon) {
         this.icon = icon;
     }
--- Proyecto/src/main/java/mx/itson/taki/entidades/Nubes.java
+++ Proyecto/src/main/java/mx/itson/taki/entidades/Nubes.java
@@ -4,9 +4,18 @@ public class Nubes {
 
     private int all;
 
+    /**
+     * Obtiene el porcentaje de nubosidad de la ciudad actual.
+     * @return El porcentaje de nubosidad de la ciudad actual.
+     */
     public int getAll() {
         return all;
     }
+    
+    /**
+     * Cambia el porcentaje de nubosidad de la ciudad actual.
+     * @param all El porcentaje de nubosidad de la ciudad actual. 
+     */
 
     public void setAll(int all) {
         this.all = all;
