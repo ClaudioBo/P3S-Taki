2c53dc7c7866a0361cf0ee8ef76b188241e80dd8
TELMEX <TELMEX@AL>
2019-11-25 21:41:13 -0600

    Documente retrofitUtil.

--- Proyecto/src/main/java/mx/itson/taki/utilerias/RetrofitUtil.java
+++ Proyecto/src/main/java/mx/itson/taki/utilerias/RetrofitUtil.java
@@ -1,22 +1,21 @@
 package mx.itson.taki.utilerias;
 
 import mx.itson.taki.interfaces.OpenWeatherAPI;
-import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitUtil {
 
+    /**
+     * Prepara el cliente para conectarse a la api. 
+     * @return El cliente para conectarse a la api 
+     */
     public static OpenWeatherAPI getApi() {
-        
-//        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-//        httpClient.setConnectTimeout$okhttp(2000);
-//        httpClient.setReadTimeout$okhttp(2000);
+    
         
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.openweathermap.org/data/2.5/")
                 .addConverterFactory(GsonConverterFactory.create())
-//                .client(httpClient.build())
                 .build();
         return retrofit.create(OpenWeatherAPI.class);
     }
